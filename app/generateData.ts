5447c262f91dc974e44e301bf2b72831d0fd82f3
ma-ray <60418739+ma-ray@users.noreply.github.com>
2023-06-15 14:34:11 -0400

    add other functions to generate options

--- app/generateData.ts
+++ app/generateData.ts
@@ -1,17 +1,43 @@
+const generateLongCall = (x: number, premium: number, strike: number) => {
+  return Math.max(x - strike, 0) - premium
+}
 
-const generateLongCall = (x: number, premium: number, strike: number, contracts: number) => {
-  if (x <= strike) {
-    return -premium * contracts
-  } else {
-    return (x - strike - premium) * contracts
-  }
+const generateLongPut = (x: number, premium: number, strike: number) => {
+  return Math.max(strike - x, 0) - premium
+}
+
+const generateShortCall = (x: number, premium: number, strike: number) => {
+  return Math.min(strike - x, 0) + premium
+}
+
+const generateShortPut = (x: number, premium: number, strike: number) => {
+  return Math.min(x - strike, 0) + premium
 }
 
 export default function generateData(position: string, type: string, strike: number, premium: number, contracts: number) {
+  let func
+  if (position === "long") {
+    if (type === "call") {
+      console.log('long call')
+      func = generateLongCall
+    } else {
+      console.log('long put')
+      func = generateLongPut
+    }
+  } else {
+    if (type === "call") {
+      console.log('short call')
+      func = generateShortCall
+    } else {
+      console.log('short put')
+      func = generateShortPut
+    }
+  }
+
   const data: number[][] = []
 
   for (let i = -1000; i < 1000; i += 0.1) {
-    data.push([i, generateLongCall(i, premium, strike, contracts)])
+    data.push([i, func(i, premium, strike) * contracts])
   }
 
   return data
